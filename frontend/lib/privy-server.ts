a3fe0ee688e83f36dfa0db28a50d148f69efb08c
Morwena0 <alpgiray0505@gmail.com>
2026-08-27 15:33:22 +0300

    feat(privy-server): sign wallet requests with PRIVY_AUTHORIZATION_PRIVATE_KEY

--- frontend/lib/privy-server.ts
+++ frontend/lib/privy-server.ts
@@ -2,5 +2,12 @@ import { PrivyClient } from '@privy-io/server-auth'
 
 const appId = process.env.NEXT_PUBLIC_PRIVY_APP_ID!
 const appSecret = process.env.PRIVY_APP_SECRET!
+const authorizationPrivateKey = process.env.PRIVY_AUTHORIZATION_PRIVATE_KEY!
 
-export const privyClient = new PrivyClient(appId, appSecret)
+if (!authorizationPrivateKey) {
+  throw new Error('PRIVY_AUTHORIZATION_PRIVATE_KEY is not set in the environment')
+}
+
+export const privyClient = new PrivyClient(appId, appSecret, {
+  walletApi: { authorizationPrivateKey },
+})
